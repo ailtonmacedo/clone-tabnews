0a620553b6250c873deb03e9b1d0bf058cb76cb1
Ailton Macedo <ailtonwsb@gmail.com>
2025-01-09 16:08:11 -0300

    move `.env` file to `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,8 +4,6 @@ async function status(request, response) {
   const result = await database.query("SELECT 1 + 1;");
   console.log(result);
   console.log("database -> " + database);
-  console.log("request -> " + request);
-  console.log("response -> " + response);
   response.status(200).json({ chave: "Alunos Acima da média" });
 }
 
