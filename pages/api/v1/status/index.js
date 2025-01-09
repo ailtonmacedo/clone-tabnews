f25e9a78be7c0f4f427ee8004cb75fed06585fea
Ailton Macedo <ailtonwsb@gmail.com>
2025-01-09 15:06:22 -0300

    adiciona `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,12 @@
-function status(request, response) {
-  console.log(response.json);
-  response.status(200).json("Alunos Acima da média");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+  console.log("database -> " + database);
+  console.log("request -> " + request);
+  console.log("response -> " + response);
+  response.status(200).json({ chave: "Alunos Acima da média" });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("GET to /api/v1/status should return 200", async () => {
+  //arrange
+
+  // act
   const response = await fetch("http://localhost:3000/api/v1/status");
+
+  //asserts
+  //Expect = Valor dinámico -> toBe = Valor Estático
   expect(response.status).toBe(200);
 });
