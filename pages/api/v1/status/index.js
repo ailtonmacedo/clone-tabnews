9ee362f9fdfb597327b6bdf0d93b4e12b788ace3
Ailton Macedo <ailtonwsb@gmail.com>
2025-01-13 13:34:12 -0300

    finaliza `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,34 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
+  console.log(request);
+
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+  const dataBaseMaxConnectionsValue = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataBaseMaxConnectionsResult =
+    dataBaseMaxConnectionsValue.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dataBaseOpenConnectionsValue =
+    dataBaseOpenConnectionsResult.rows[0].count;
+
   response.status(200).json({
     update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsResult),
+        opened_connections: dataBaseOpenConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,13 +2,16 @@
 // act
 //Expect = Valor dinámico -> toBe = Valor Estático
 
-test("GET to /api/v1/status should return 200", async () => {
+test("Teste - GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
 
   const parseUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parseUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.6");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
