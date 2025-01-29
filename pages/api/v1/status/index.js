8f37feec18ca6ee710a292acccf618efe9209408
Ailton Macedo <ailtonwsb@gmail.com>
2025-01-29 17:23:12 -0300

    refactor

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,6 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  let variavelEsquecida;
   const updateAt = new Date().toISOString();
 
   const dataBaseVersionResult = await database.query("SHOW server_version;");
