21f9b562668ee2a769979d75f69a41773342b723
Ailton Macedo <ailtonwsb@gmail.com>
2025-02-14 15:09:09 -0300

    refactor: page `status`

--- pages/status/index.js
+++ pages/status/index.js
@@ -24,37 +24,36 @@ function UpdatedAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 100,
   });
-  console.log(data);
 
-  let updatedAtText = "Carregando...";
+  let updatedAtText;
   let databaseVersion;
   let databaseMaxConnections;
   let databaseOpenedConnections;
-  let loading = "Carregando...";
 
   if (!isLoading && data) {
     updatedAtText = new Date(data.update_at).toLocaleString("pt-BR");
     databaseVersion = data.dependencies.database.version;
     databaseMaxConnections = data.dependencies.database.max_connections;
     databaseOpenedConnections = data.dependencies.database.opened_connections;
+
+    let div = (
+      <div>
+        <div>
+          <h3>Última Atualização:</h3>
+          <div>Date e Hora: {updatedAtText}</div>
+        </div>
+        <div>
+          <h3>Banco de Dados:</h3>
+          <ul>
+            <li>Versão: {databaseVersion}</li>
+            <li>Máximo de Conexões: {databaseMaxConnections}</li>
+            <li>Conexões Abertas: {databaseOpenedConnections}</li>
+          </ul>
+        </div>
+      </div>
+    );
+    return div;
+  } else {
+    return <div>Carregando...</div>;
   }
-
-  let div = (
-    <div>
-      <div>
-        <h3>Última Atualização:</h3>
-        <div>Date e Hora: {updatedAtText}</div>
-      </div>
-      <div>
-        <h3>Banco de Dados:</h3>
-        <ul>
-          <li>Versão: {databaseVersion || loading}</li>
-          <li>Máximo de Conexões: {databaseMaxConnections || loading}</li>
-          <li>Conexões Abertas: {databaseOpenedConnections || loading}</li>
-        </ul>
-      </div>
-    </div>
-  );
-
-  return div;
 }
